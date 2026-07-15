01713ece018ab03345bf45a7955c5d5fa736e480
N4ic0 <naico@users.noreply.github.com>
2026-07-15 17:16:50 -0400

    backup: respaldo general del proyecto antes de migración a Bootstrap 5 + DataTables

--- src/resources/views/admin/productos/pdf/catalogo.blade.php
+++ src/resources/views/admin/productos/pdf/catalogo.blade.php
@@ -4,54 +4,54 @@
 <meta charset="utf-8">
 <style>
 @page {
-    margin: 18mm 12mm 12mm 12mm;
+    margin: 10mm 8mm 8mm 8mm;
 }
 
 body {
-    font-family: 'serif';
+    font-family: 'DejaVu Serif', serif;
     color: #1a1a1a;
-    font-size: 10pt;
-    line-height: 1.4;
+    font-size: 9pt;
+    line-height: 1.3;
 }
 
 .watermark {
     position: fixed;
     top: 50%;
     left: 50%;
-    margin-left: -200px;
-    margin-top: -200px;
+    margin-left: -175px;
+    margin-top: -175px;
     opacity: 0.06;
     z-index: -1;
 }
 
 .header {
     text-align: center;
-    padding-bottom: 4mm;
-    margin-bottom: 4mm;
-    border-bottom: 2px solid #c9a84c;
+    padding-bottom: 2mm;
+    margin-bottom: 3mm;
+    border-bottom: 1.5px solid #c9a84c;
 }
 
 .header h1 {
     color: #111;
-    font-size: 18pt;
+    font-size: 16pt;
     margin: 0;
-    letter-spacing: 4px;
+    letter-spacing: 3px;
     text-transform: uppercase;
     font-weight: 400;
 }
 
 .header .hotel {
-    color: #666;
-    font-size: 8pt;
-    letter-spacing: 2px;
-    margin-top: 2mm;
+    color: #111;
+    font-size: 7.5pt;
+    letter-spacing: 1.5px;
+    margin-top: 1.5mm;
 }
 
 .header .fecha {
-    color: #888;
-    font-size: 7pt;
+    color: #555;
+    font-size: 6.5pt;
     letter-spacing: 1px;
-    margin-top: 1mm;
+    margin-top: 0.5mm;
 }
 
 .columns {
@@ -70,34 +70,37 @@ body {
 }
 
 .category-title {
-    font-size: 11pt;
-    color: #c9a84c;
+    font-size: 10pt;
+    color: #111;
     text-transform: uppercase;
-    letter-spacing: 2px;
-    margin-top: 3mm;
-    margin-bottom: 2mm;
-    padding-bottom: 0.5mm;
-    border-bottom: 1px solid #e0d5b8;
+    letter-spacing: 1.5px;
+    margin-top: 2mm;
+    margin-bottom: 1mm;
+    padding-bottom: 0.3mm;
+    border-bottom: 0.8px solid #c9a84c;
 }
 
-.product-row {
-    padding: 0.8mm 0;
-    border-bottom: 1px dotted #ece5d5;
-    overflow: hidden;
+.prodtable {
+    width: 100%;
+    border-collapse: collapse;
+    margin-bottom: 0.5mm;
 }
-
-.product-name {
-    font-size: 9pt;
-    color: #1a1a1a;
-    float: left;
-    max-width: 70%;
+.prodtable tr {
+    border-bottom: 0.3px solid #e0d8c8;
 }
-
-.product-price {
-    font-size: 9pt;
+.prodname {
+    font-size: 8.5pt;
+    color: #111;
+    padding: 0.3mm 0;
+    text-align: left;
+}
+.prodprice {
+    font-size: 8.5pt;
     color: #c9a84c;
     font-weight: bold;
-    float: right;
+    padding: 0.3mm 0 0.3mm 2mm;
+    text-align: right;
+    white-space: nowrap;
 }
 
 .footer {
@@ -122,10 +125,10 @@ body {
     padding: 0;
 }
 .pdftable .left-col {
-    padding-right: 5mm;
+    padding-right: 4mm;
 }
 .pdftable .right-col {
-    padding-left: 5mm;
+    padding-left: 4mm;
 }
 
 .page-break {
@@ -136,7 +139,7 @@ body {
 <body>
 
 <div class="watermark">
-    <img src="{{ $iconoPath }}" width="400">
+    <img src="{{ $iconoPath }}" width="350">
 </div>
 
 <div class="header">
@@ -146,31 +149,48 @@ body {
 </div>
 
 @php
-    $leftCat = 'Alcohol';
-    $leftItems = $productos->get($leftCat, collect());
-    $rightCats = $productos->except($leftCat);
+    $total = $productos->sum(fn($i) => $i->count());
+    $half = intval(ceil($total / 2));
+    $leftCats = collect();
+    $rightCats = collect();
+    $acc = 0;
+    foreach ($productos as $cat => $items) {
+        $cnt = $items->count();
+        if ($acc < $half) {
+            $leftCats[$cat] = $items;
+        } else {
+            $rightCats[$cat] = $items;
+        }
+        $acc += $cnt;
+    }
 @endphp
 
 <table class="pdftable">
 <tr>
 <td class="left-col">
-    <div class="category-title">{{ $leftCat }}</div>
-    @foreach($leftItems as $producto)
-    <div class="product-row">
-        <span class="product-name">{{ $producto->nombre }}</span>
-        <span class="product-price">${{ number_format($producto->precio, 0, ',', '.') }}</span>
-    </div>
+    @foreach($leftCats as $categoria => $items)
+    <div class="category-title">{{ $categoria }}</div>
+    <table class="prodtable">
+        @foreach($items as $producto)
+        <tr>
+            <td class="prodname">{{ $producto->nombre }}</td>
+            <td class="prodprice">${{ number_format($producto->precio, 0, ',', '.') }}</td>
+        </tr>
+        @endforeach
+    </table>
     @endforeach
 </td>
 <td class="right-col">
     @foreach($rightCats as $categoria => $items)
     <div class="category-title">{{ $categoria }}</div>
+    <table class="prodtable">
         @foreach($items as $producto)
-        <div class="product-row">
-            <span class="product-name">{{ $producto->nombre }}</span>
-            <span class="product-price">${{ number_format($producto->precio, 0, ',', '.') }}</span>
-        </div>
+        <tr>
+            <td class="prodname">{{ $producto->nombre }}</td>
+            <td class="prodprice">${{ number_format($producto->precio, 0, ',', '.') }}</td>
+        </tr>
         @endforeach
+    </table>
     @endforeach
 </td>
 </tr>
